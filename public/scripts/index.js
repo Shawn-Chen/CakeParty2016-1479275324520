8d396e1a9c607af87af37c7c53c220a033e941da
Hsiang Wen <shiyoubun@gmail.com>
2016-11-22 17:59:44 +0800

    all

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,16 @@
 $(function() {
-	$("#q1").on('click', function(e) {
-		e.preventDefault();
-		$('#page').load("q1.html");
-	});
+  $('a[href="#posQ1"]').click(function(){
+  	$('.layout_q1').css('display', 'inline');
+  	$('.row').css('display', 'none');
+  });
+  $(".layout_q1 [href='#top']").click(function(){
+  	$('.layout_q1').css('display', 'none');
+  	$('.row').css('display', 'inline');
+  });
+  $("#btn-scoreboard").click(function(){
+ 	$('#scorepage').toggle();
+  });
+  $("#btn-reset").click(function(){
+
+  });
 });
