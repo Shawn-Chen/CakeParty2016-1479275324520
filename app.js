0f4253b831662259b4267973ba8f36e1f4974876
Hsiang Wen <shiyoubun@gmail.com>
2016-11-23 10:04:54 +0800

    all

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ var express  = require('express'),
   extend     = require('util')._extend,
   watson     = require('watson-developer-cloud');
 
+// Bootstrap application settings
+require('./config/express')(app);
+
 // all environments
 app.use('/api/speech-to-text/', require('./stt-token.js'));
 app.use('/api/text-to-speech/', require('./tts-token.js'));
 
-
+// error-handler settings
+require('./config/error-handler')(app);
 
 var port = process.env.VCAP_APP_PORT || 3000;
 app.listen(port);
